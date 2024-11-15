1b70cf9999acd64f6f09bd91e5efb6f10b0e4ebc
MIGUEL ANGEL DOMINGUEZ SERRANO <miguel.dominguez@catsaconcretos.mx>
2024-11-15 17:43:31 -0600

    Reportes Graficas
    Reportes por grafica Pedido

--- src/views/widgets/WidgetsDropdown.js
+++ src/views/widgets/WidgetsDropdown.js
@@ -11,7 +11,7 @@ import {
   CWidgetStatsA,
 } from '@coreui/react'
 import { getStyle } from '@coreui/utils'
-import { CChartBar, CChartLine } from '@coreui/react-chartjs'
+import { CChartBar, CChartLine, CChartPolarArea } from '@coreui/react-chartjs'
 import CIcon from '@coreui/icons-react'
 import { cilArrowBottom, cilArrowTop, cilOptions } from '@coreui/icons'
 
@@ -35,7 +35,49 @@ const WidgetsDropdown = (props) => {
         })
       }
     })
+    getCotizaciones();
   }, [widgetChartRef1, widgetChartRef2])
+
+  async function getCotizaciones()
+  {
+    try
+    {
+      let confi_ax = {
+        headers:
+        {
+            'Cache-Control': 'no-cache',
+            'Content-Type': 'application/json',
+            "Authorization": "Bearer "+cookies.get('token'),
+        },
+      };
+      //------------------------------------------------------------------------------------------------------------------------------------------------------
+      const response = await axios.get(baseUrl2+'Operaciones/GetCotizaciones', confi_ax);
+      const obj = response.data[0].Rows;
+      const labels = [];
+      const dataSet = [];
+      obj.forEach(item => {
+        labels.push(item.Planta);
+        dataSet.push(item.TotalPedidos);
+      });
+      // Actualiza el estado con los nuevos datos
+      setChartDataD({
+        labels: labels,
+        datasets: [
+          {
+            data: dataSet,
+            backgroundColor: ['#FF6384', '#4BC0C0', '#FFCE56', '#E7E9ED', '#36A2EB'],
+          },
+        ],
+      });
+      console.log(obj)
+    }
+    catch(error)
+    {
+      Swal.fire("Error", "Ocurrio un error, vuelva a intentarlo", "error");
+    }finally{
+
+    }
+  }
 
   return (
     <CRow className={props.className} xs={{ gutter: 4 }}>
@@ -153,6 +195,82 @@ const WidgetsDropdown = (props) => {
           }
           chart={
             <CChartLine
+              ref={widgetChartRef2}
+              className="mt-3 mx-3"
+              style={{ height: '70px' }}
+              data={{
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [
+                  {
+                    label: 'My First dataset',
+                    backgroundColor: 'transparent',
+                    borderColor: 'rgba(255,255,255,.55)',
+                    pointBackgroundColor: getStyle('--cui-info'),
+                    data: [1, 18, 9, 17, 34, 22, 11],
+                  },
+                ],
+              }}
+              options={{
+                plugins: {
+                  legend: {
+                    display: false,
+                  },
+                },
+                maintainAspectRatio: false,
+                scales: {
+                  x: {
+                    border: {
+                      display: false,
+                    },
+                    grid: {
+                      display: false,
+                      drawBorder: false,
+                    },
+                    ticks: {
+                      display: false,
+                    },
+                  },
+                  y: {
+                    min: -9,
+                    max: 39,
+                    display: false,
+                    grid: {
+                      display: false,
+                    },
+                    ticks: {
+                      display: false,
+                    },
+                  },
+                },
+                elements: {
+                  line: {
+                    borderWidth: 1,
+                  },
+                  point: {
+                    radius: 4,
+                    hitRadius: 10,
+                    hoverRadius: 4,
+                  },
+                },
+              }}
+            />
+          }
+        />
+      </CCol>
+      <CCol sm={6} xl={4} xxl={3}>
+      <CWidgetStatsA
+          color="danger"
+          value={
+            <>
+              $6.200{' '}
+              <span className="fs-6 fw-normal">
+                (40.9% <CIcon icon={cilArrowTop} />)
+              </span>
+            </>
+          }
+          title="Pedidos"
+          chart={
+            <CChartPolarArea
               ref={widgetChartRef2}
               className="mt-3 mx-3"
               style={{ height: '70px' }}
