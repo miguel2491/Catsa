7d74369f45137eac57d3e02bde4e8a7542ccf3a0
MIGUEL ANGEL DOMINGUEZ SERRANO <miguel.dominguez@catsaconcretos.mx>
2024-11-29 17:44:45 -0600

    RepPedidos
    Vista de Pedidos Individual

--- src/views/reportes/RBPedido.js
+++ src/views/reportes/RBPedido.js
@@ -20,7 +20,8 @@ import {
 
 import {CIcon} from '@coreui/icons-react'
 import { cilSearch } from '@coreui/icons';
-import {getPedidoInd} from '../../Utilidades/Funciones'
+import {getPedidoInd, getObraInd} from '../../Utilidades/Funciones'
+import {TiempoT, Fnum} from '../../Utilidades/Tools'
 
 const RBPedido = () => {
   const [loading, setLoading] = useState(false);
@@ -38,9 +39,9 @@ const RBPedido = () => {
       bomba:'',
       fcaEntrega:'',
       hraSalida:'',
-      tRecorrido:'',
+      tRecorrido:0,
       tRealAprox:'',
-      espaciado:'',
+      espaciado:0,
       estatus:'',
       pProducto:'',
       iTotalPagar:'',
@@ -52,6 +53,23 @@ const RBPedido = () => {
       actualizo:'',
       aVBueno:'',
   })
+  const [DataObra, setDataObra] = useState({
+    Obra: '',
+    utm: '',
+    nvendedor: '',
+    vendedor: '',
+    nocliente:'',
+    cliente:'',
+    Direccion:'',
+    bloqueado:false,
+    alkon:false,
+    calle:'',
+    ciudad:'',
+    telefono:'',
+    postal:'',
+    solicita:'',
+    descripcionP:''
+})
   const handleInputChange = (e) => {
     setNPedido(e.target.value); // Actualiza el estado con el nuevo valor del input
   };
@@ -89,9 +107,29 @@ const RBPedido = () => {
       const data = await getPedidoInd(nPedido);
       console.log(data[0]);
       if (data) {
-        setDataPedido(data[0]); // Aquí se asigna el primer objeto del array (suponiendo que es el único)
+        setDataPedido(data[0]);
+        var planta = data[0].Planta;
+        var obra = data[0].NoObra;
+        getDObra(obra,planta);
       } else {
         Swal.fire("No se encontraron datos", "El número de pedido no es válido", "error");
+        setLoading(false); // Oculta el modal de carga
+        setVisible(false); // Cierra el modal de carga
+      }
+    } catch (error) {
+      console.error(error);
+      Swal.fire("Error", "Hubo un problema al obtener los datos", "error");
+    } 
+  }
+
+  const getDObra = async (nObra, planta) => {
+    try {
+      const dataObra = await getObraInd(nObra, planta);
+      console.log(dataObra[0]);
+      if (dataObra) {
+        setDataObra(dataObra[0]); // Aquí se asigna el primer objeto del array (suponiendo que es el único)
+      } else {
+        Swal.fire("No se encontraron datos", "Obra NO válida", "error");
       }
     } catch (error) {
       console.error(error);
@@ -101,37 +139,38 @@ const RBPedido = () => {
       setVisible(false); // Cierra el modal de carga
     }
   }
-
-  useEffect(()=>{    
-    if(fetchedData)
-    {
-      // Asignar los valores a las constantes del estado
-      setPlanta(fetchedData.Planta);
-      setAsesor(fetchedData.asesor);
-      setProgramado(fetchedData.activo);
-      setVBueno(fetchedData.VistoBueno);
-      setObra(fetchedData.NoObra);
-      setDireccion(fetchedData.direccion);
-      setProducto(fetchedData.Producto);
-      setM3(fetchedData.M3Viaje);
-      setBomba(fetchedData.CodBomba);
-      setFcaEntrega(fetchedData.FechaHoraPedido);
-      setHraSalida(fetchedData.hraSalida);
-      setRecorrido(fetchedData.TRecorrido);
-      setRAprox(fetchedData.TiempoReal);
-      setEspaciado(fetchedData.Espaciado);
-      setEstatus(fetchedData.estatus);
-      setPProducto(fetchedData.PrecioProducto);
-      setTotalPagar(fetchedData.iTotalPagar);
-      setAnticipo(fetchedData.sAnticipo);
-      setMBReal(fetchedData.mBrutoReal);
-      setFPago(fetchedData.Pago);
-      setObserv(fetchedData.Observaciones);
-      setCreo(fetchedData.UsuarioCreo);
-      setActualizo(fetchedData.UsuarioActualizo);
-      setAVBueno(fetchedData.UsuarioCyC);
+  useEffect(() => {
+    if (fetchedData && DataObra) {
+      const fcaEntrega = fetchedData.FechaHoraPedido ?? '';
+      // Usar split para separar la fecha y la hora
+      const [fecha, hora] = fcaEntrega.split("T");
+      const obra_ = (fetchedData.NoObra ?? '')+ ' ' + (DataObra.Obra ?? '');
+      setPlanta(fetchedData.Planta ?? '');
+      setAsesor(DataObra.Vendedor ?? '');
+      setProgramado(fetchedData.activo ?? false);
+      setVBueno(fetchedData.VistoBueno ?? false);
+      setObra(obra_);
+      setDireccion(DataObra.Direccion ?? '');
+      setProducto(fetchedData.Producto ?? '');
+      setM3(fetchedData.M3Viaje ?? '');
+      setBomba(fetchedData.CodBomba ?? '');
+      setFcaEntrega(fecha ?? '');
+      setHraSalida(hora ?? '');
+      setRecorrido(TiempoT(fetchedData.TRecorrido ?? 0) ?? 0);
+      setRAprox(fetchedData.TiempoReal ?? '');
+      setEspaciado(TiempoT(fetchedData.Espaciado ?? 0) ?? 0);
+      setEstatus(fetchedData.estatus ?? '');
+      setPProducto(fetchedData.PrecioProducto ?? '');
+      setTotalPagar(fetchedData.Total ?? '');
+      setAnticipo(fetchedData.sAnticipo ?? '');
+      setMBReal(fetchedData.mBrutoReal ?? '');
+      setFPago(fetchedData.Pago ?? '');
+      setObserv(fetchedData.Observaciones ?? '');
+      setCreo(fetchedData.UsuarioCreo ?? '');
+      setActualizo(fetchedData.UsuarioActualizo ?? '');
+      setAVBueno(fetchedData.UsuarioCyC ?? '');
     }
-  },[fetchedData]);
+  }, [fetchedData, DataObra]);
 
   return (
     <>
@@ -167,7 +206,7 @@ const RBPedido = () => {
                             <CFormInput type='text' id='asesor' label='Asesor' disabled value={asesor} />
                           </CCol>
                         </CRow>
-                        <CRow>
+                        <CRow className='mt-4'>
                           <CCol xs={6} md={3}>
                             <CFormInput type='text' id='obra' label='Obra' disabled value={obra} />
                           </CCol>
@@ -178,10 +217,10 @@ const RBPedido = () => {
                             <CFormInput type='text' id='producto' label='Producto' disabled value={producto} />
                           </CCol>
                           <CCol xs={6} md={3}>
-                            <CFormInput type='text' id='m3' label='M3' disabled value={m3}/>
+                            <CFormInput type='text' id='m3' label='M3/Viaje' disabled value={m3}/>
                           </CCol>
                         </CRow>
-                        <CRow>
+                        <CRow className='mt-4'>
                           <CCol xs={6} md={3}>
                             <CFormInput type='text' id='bomba' label='Bomba' disabled value={bomba} />
                           </CCol>
@@ -195,35 +234,35 @@ const RBPedido = () => {
                             <CFormInput type='text' id='tRecorrido' label='Tiempo Recorrido' disabled value={tRecorrido}/>
                           </CCol>
                         </CRow>
-                        <CRow>
+                        <CRow className='mt-4'>
                           <CCol xs={6} md={3}>
                             <CFormInput type='text' id='tReal' label='Tiempo Real Aproximado' disabled value={tRealAprox}/>
                           </CCol>
                           <CCol xs={6} md={3}>
                             <CFormInput type='text' id='espaciado' label='Espaciado' disabled value={espaciado}/>
                           </CCol>
-                          <CCol xs={6} md={3}>
+                          {/* <CCol xs={6} md={3}>
                             <CFormInput type='text' id='estatus' label='Estatus' disabled value={estatus}/>
-                          </CCol>
+                          </CCol> */}
                           <CCol xs={6} md={3}>
                             <CFormInput type='text' id='pProducto' label='Precio Producto' disabled value={pProducto}/>
                           </CCol>
                         </CRow>
-                        <CRow>
+                        <CRow className='mt-4'>
                           <CCol xs={6} md={3}>
                             <CFormInput type='text' id='iTotalPagar' label='Importe Total a Pagar' disabled value={iTotalPagar} />
                           </CCol>
                           <CCol xs={6} md={3}>
                             <CFormInput type='text' id='sAnticipo' label='Saldo Anticipo' disabled value={sAnticipo}/>
                           </CCol>
-                          <CCol xs={6} md={3}>
+                          {/* <CCol xs={6} md={3}>
                             <CFormInput type='text' id='mBrutoReal' label='Margen Bruto Real' disabled value={mBrutoReal} />
-                          </CCol>
+                          </CCol> */}
                           <CCol xs={6} md={3}>
                             <CFormInput type='text' id='fPago' label='Forma de Pago' disabled value={fPago} />
                           </CCol>
                         </CRow>
-                        <CRow>
+                        <CRow className='mt-4'>
                           <CCol xs={6} md={3}>
                             <CFormInput type='text' id='obs' label='Observaciones' disabled value={observaciones} />
                           </CCol>
